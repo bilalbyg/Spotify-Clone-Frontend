5e0b6fad4ae4ea1bb7677df7c28c9a28877b750e
bilalmevlutbeygo <bbeygo.71@gmail.com>
2026-03-13 00:11:19 +0300

    chore(frontend): initialize new project configuration and package.json

--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-function secondsToTime(seconds) {
-  const date = new Date(null);
-  date.setSeconds(seconds); // specify value for SECONDS here
-  const result = date.toISOString().slice(14, 19);
-  return result;
-}
-
-export { secondsToTime };
